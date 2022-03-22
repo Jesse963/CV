9c988c67393f2b1b1cc3a49fb5c7784026473e63
Jesse963 <jesse-jenkins@hotmail.com>
2022-03-22 22:01:21 +1100

    styling

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 const header = document.querySelector(".header.name");
 const content = document.querySelector(".main.content h2");
+const burgerIcon = document.querySelector("header svg");
+
+console.log(burgerIcon);
 
 const observer = new IntersectionObserver(
   (entries, observer) => {
+    const nav = document.querySelector("header nav ");
     if (!entries[0].isIntersecting) {
-      header.style.fontSize = "1.5rem";
-    } else {
-      header.style.fontSize = "2.7rem";
+      header.style.fontSize = "1.6rem";
+      nav.style.maxHeight = "4rem";
+      return (burgerIcon.style.width = "1.6rem");
     }
-    console.log(entries[0].isIntersecting);
-    console.log(entries[0].intersectionRatio);
+    console.log("Test");
+    burgerIcon.style.width = "2.6rem";
+    header.style.fontSize = "2.6rem";
+    nav.style.maxHeight = "7rem";
   },
   { rootMargin: "-200px" }
 );
 
 console.log(observer);
 observer.observe(content);
+
+burgerIcon.addEventListener("click", () => {
+  const links = document.querySelector("header nav .links");
+  const nav = document.querySelector("header nav ");
+
+  if (links.style.opacity === "1") {
+    links.style.opacity = "0";
+    links.style.pointerEvents = "none";
+    return (nav.style.maxHeight = "7rem");
+  }
+  // links.style.display = "flex";
+  links.style.pointerEvents = "all";
+  nav.style.maxHeight = "25rem";
+  setTimeout(() => {
+    links.style.opacity = "1";
+  }, 100);
+});
